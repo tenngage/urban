fff5992e2691665b65c14285ce132aebb0e7fdda
Ilya <144267633+tenngage@users.noreply.github.com>
2024-12-01 22:25:37 +0300

    Added variables.py

---
+++ variables.py
@@ -0,0 +1,5 @@
+completed_homeworks = 12
+hours_spended =  1.5
+course_name = "Python"
+time_for_one_homework = 1.5 / 12
+print(f"Курс: {course_name}, всего задач: {completed_homeworks}, затрачено часов: {hours_spended}, среднее время выполнения: {time_for_one_homework}")
