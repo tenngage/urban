9976f7f7f5eca9b3af654ff8558dc44e2aa16f35
tenngage <ibi0880@gmail.com>
2024-12-14 16:35:36 +0300

    Added module_2_5.py

---
+++ module_2_5.py
@@ -0,0 +1,8 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
+    return matrix
+print(get_matrix(5, 5, 10))
