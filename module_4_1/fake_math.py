71324948e5b4841796aa3903bc22059d5b6a13d3
Ilya <144267633+tenngage@users.noreply.github.com>
2025-01-10 05:44:14 +0300

    Add files via upload

---
+++ module_4_1/fake_math.py
@@ -0,0 +1,5 @@
+def divide(first, second):
+    if second == 0:
+         return "Ошибка"
+    res = first / second
+    return res
---
+++ module_4_1/module_4_1.py
@@ -0,0 +1,6 @@
+from fake_math import divide as fake_divide
+from true_math import divide as true_divide
+
+print(fake_divide(15, 5))
+print(fake_divide(15, 0))
+print(true_divide(15, 0))
