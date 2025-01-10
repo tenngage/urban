0d51ac1791e45402dd11a70694888b17a2491c9f
Ilya <144267633+tenngage@users.noreply.github.com>
2025-01-10 08:01:21 +0300

    Add files via upload

---
+++ module_5_1.py
@@ -0,0 +1,7 @@
+def test_function():
+    def inner_function():
+        print("Я в области видимости test_function")
+    inner_function()
+
+test_function() # Я в области видимости test_function
+# inner_function() Ошибка
